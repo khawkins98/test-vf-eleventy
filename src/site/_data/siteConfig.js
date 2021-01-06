081b54391b978fa45582b67a0573e221ed6e03a7
Nitin Jadhav <nitin@ebi.ac.uk>
2021-01-06 17:41:03 +0530

    Initial commit

--- src/site/_data/siteConfig.js
+++ src/site/_data/siteConfig.js
@@ -1,10 +1,10 @@
 // Some various reusable configuration
 module.exports = {
   siteInformation: {
-    title: "Eleventy for the VF 2.0",
-    short_description: 'This allows using the <a class="vf-link" href="https://www.11ty.io">11ty</a> static site generator with direct access to <a class="vf-link" href="https://github.com/visual-framework/vf-core">Visual Framework 2.0</a> components.',
-    url: "https://visual-framework.github.io/vf-eleventy/",
-    author: "Visual Framework system",
-    email: "ken.hawkins@embl.de",
+    title: "EMBL-EBI Security Hall of Fame",
+    short_description: 'List of users who identified security issues in EMBL-EBI websites',
+    url: "http://10.49.3.142:31898/security/hall-of-fame.html",
+    author: "EBI Webdev team",
+    email: "nitin@ebi.ac.uk",
   }
 };
